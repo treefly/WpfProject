16bae660c987893e9927129faeb16418c26a3996
hailun.zhang <hailun.zhang@toshiba-tih.com>
2018-11-16 17:23:46 +0800

    Data binding style

---
+++ WpfExample/WpfExample/Data/ExampleBindingCollection.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfExample.Data
+{
+    /// <summary>
+    /// ExampleBindingCollection.xaml 的交互逻辑
+    /// </summary>
+    public partial class ExampleBindingCollection : Window
+    {
+        public  ObservableCollection<Person> _persons = new ObservableCollection<Person>()
+        {
+            new Person {FirstName = "1", LastName = "2", HomeTown = "3"},
+            new Person {FirstName = "4", LastName = "5", HomeTown = "6"},
+            new Person {FirstName = "7", LastName = "8", HomeTown = "9"},
+            new Person {FirstName = "10", LastName = "11", HomeTown = "12"},
+        };
+        public ExampleBindingCollection()
+        {
+            InitializeComponent();
+        }
+      
+    }
+}
---
+++ WpfExample/WpfExample/Data/ExampleBindingDirection.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfExample.Data
+{
+    /// <summary>
+    /// ExampleBindingDirection.xaml 的交互逻辑
+    /// </summary>
+    public partial class ExampleBindingDirection : Window
+    {
+        /*
+         *  你使用Mode属性指定绑定的方向。 以下枚举列表显示了可供绑定更新的选项：
+            TwoWay 当发生更改时的目标属性或源属性更新目标属性。
+            OneWay 仅当源属性更改时，请更新目标属性。
+            OneTime 仅当应用程序启动时或时，请更新目标属性DataContext发生了更改。
+            OneWayToSource 目标属性更改时，请更新源属性。
+            Default 默认值将导致Mode要使用的目标属性的值。
+         */
+        public ExampleBindingDirection()
+        {
+            InitializeComponent();
+        }
+    }
+}
