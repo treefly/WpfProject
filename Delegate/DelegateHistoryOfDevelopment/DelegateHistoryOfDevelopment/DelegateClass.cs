65282b4ecec7e2b53fef6f4c05239a8333d97d5c
hailun.zhang <hailun.zhang@toshiba-tih.com>
2019-01-18 16:56:24 +0800

    add project

---
+++ Delegate/DelegateHistoryOfDevelopment/DelegateHistoryOfDevelopment/DelegateClass.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegateHistoryOfDevelopment
+{
+    public class DelegateClass
+    {
+
+        public delegate int AddDelegate(int numA,int numB);
+        public DelegateClass()
+        {
+            //版本1
+            AddDelegate addDelegate = new AddDelegate(Add);
+            int c = addDelegate(10,20);
+
+            //匿名方法
+            AddDelegate addTwo = new AddDelegate(delegate(int a, int b) { return a + b;});
+
+            //不定义委托用Func和Action
+            Func<int, int, int> func = new Func<int, int, int>(delegate(int a, int b) { return a + b; });
+
+            //再次省些一下
+            Func<int, int, int> func2 = delegate(int a, int b) { return a + b; };
+
+            //匿名方法进化为lambda表达式
+            AddDelegate lambda = new AddDelegate((int a, int b) => a + b);
+            AddDelegate lambda2 = new AddDelegate((a, b) => a + b);
+        }
+
+        public int Add(int num1,int num2)
+        {
+            return num1 + num2;
+        }
+    }
+}
---
+++ Task/TaskConsole/TaskConsole/Program.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TaskConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Main();
+            Console.WriteLine("Finished {0} loop iterations", "11");
+            Console.ReadLine();
+        }
+        public static async Task Main()
+        {
+           await Task.Run(() => {
+                // Just loop.
+                int ctr = 0;
+                for (ctr = 0; ctr <= 1000000; ctr++)
+                { }
+                Console.WriteLine("Finished {0} loop iterations", ctr);
+            });
+        }
+
+        public static void  StartNewSecond()
+        {
+            Task t = Task.Factory.StartNew(() => {
+                // Just loop.
+                int ctr = 0;
+                for (ctr = 0; ctr <= 1000000; ctr++)
+                { }
+                Console.WriteLine("Finished {0} loop iterations",
+                    ctr);
+            });
+            t.Wait();
+        }
+
+        public static void Wait()
+        {
+            // Wait on a single task with no timeout specified.
+            Task taskA = Task.Run(() => Thread.Sleep(2000));
+            Console.WriteLine("taskA Status: {0}", taskA.Status);
+            try
+            {
+                taskA.Wait();
+                Console.WriteLine("taskA Status: {0}", taskA.Status);
+            }
+            catch (AggregateException)
+            {
+                Console.WriteLine("Exception in taskA.");
+            }
+        }
+
+        public static void WaitSecondTime()
+        {
+            // Wait on a single task with a timeout specified.
+            Task taskA = Task.Run(() => Thread.Sleep(2000));
+            try
+            {
+                taskA.Wait(1000);       // Wait for 1 second.
+                bool completed = taskA.IsCompleted;
+                Console.WriteLine("Task A completed: {0}, Status: {1}", completed, taskA.Status);
+                if (!completed)
+                    Console.WriteLine("Timed out before task A completed.");
+            }
+            catch (AggregateException)
+            {
+                Console.WriteLine("Exception in taskA.");
+            }
+        }
+
+        public void WaitAnyTask()
+        {
+            var tasks = new Task[3];
+            var rnd = new Random();
+            for (int ctr = 0; ctr <= 2; ctr++)
+                tasks[ctr] = Task.Run(() => Thread.Sleep(rnd.Next(500, 3000)));
+            try
+            {
+                int index = Task.WaitAny(tasks);
+                Console.WriteLine("Task #{0} completed first.\n", tasks[index].Id);
+                Console.WriteLine("Status of all tasks:");
+                foreach (var t in tasks)
+                    Console.WriteLine("   Task #{0}: {1}", t.Id, t.Status);
+            }
+            catch (AggregateException)
+            {
+                Console.WriteLine("An exception occurred.");
+            }
+        }
+
+        public void WaitAllTaskFinish()
+        {
+            // Wait for all tasks to complete.
+            Task[] tasks = new Task[10];
+            for (int i = 0; i < 10; i++)
+            {
+                tasks[i] = Task.Run(() => Thread.Sleep(2000));
+            }
+            try
+            {
+                Task.WaitAll(tasks);
+            }
+            catch (AggregateException ae)
+            {
+                Console.WriteLine("One or more exceptions occurred: ");
+                foreach (var ex in ae.Flatten().InnerExceptions)
+                    Console.WriteLine("   {0}", ex.Message);
+            }
+
+            Console.WriteLine("Status of completed tasks:");
+            foreach (var t in tasks)
+                Console.WriteLine("   Task #{0}: {1}", t.Id, t.Status);
+        }
+
+        public void TaskCatchException()
+        {
+            // Create a cancellation token and cancel it.
+            var source1 = new CancellationTokenSource();
+            var token1 = source1.Token;
+            source1.Cancel();
+            // Create a cancellation token for later cancellation.
+            var source2 = new CancellationTokenSource();
+            var token2 = source2.Token;
+
+            // Create a series of tasks that will complete, be cancelled, 
+            // timeout, or throw an exception.
+            Task[] tasks = new Task[12];
+            for (int i = 0; i < 12; i++)
+            {
+                switch (i % 4)
+                {
+                    // Task should run to completion.
+                    case 0:
+                        tasks[i] = Task.Run(() => Thread.Sleep(2000));
+                        break;
+                    // Task should be set to canceled state.
+                    case 1:
+                        tasks[i] = Task.Run(() => Thread.Sleep(2000),
+                                 token1);
+                        break;
+                    case 2:
+                        // Task should throw an exception.
+                        tasks[i] = Task.Run(() => { throw new NotSupportedException(); });
+                        break;
+                    case 3:
+                        // Task should examine cancellation token.
+                        tasks[i] = Task.Run(() => {
+                            Thread.Sleep(2000);
+                            if (token2.IsCancellationRequested)
+                                token2.ThrowIfCancellationRequested();
+                            Thread.Sleep(500);
+                        }, token2);
+                        break;
+                }
+            }
+            Thread.Sleep(250);
+            source2.Cancel();
+
+            try
+            {
+                Task.WaitAll(tasks);
+            }
+            catch (AggregateException ae)
+            {
+                Console.WriteLine("One or more exceptions occurred:");
+                foreach (var ex in ae.InnerExceptions)
+                    Console.WriteLine("   {0}: {1}", ex.GetType().Name, ex.Message);
+            }
+
+            Console.WriteLine("\nStatus of tasks:");
+            foreach (var t in tasks)
+            {
+                Console.WriteLine("   Task #{0}: {1}", t.Id, t.Status);
+                if (t.Exception != null)
+                {
+                    foreach (var ex in t.Exception.InnerExceptions)
+                        Console.WriteLine("      {0}: {1}", ex.GetType().Name,
+                                          ex.Message);
+                }
+            }
+        }
+    }
+
+    }
+}
